ac2177b9ff3001fd9ffa3a1e6562cc34b0af8bb8
Ignat Georgiev <ignat.m.georgiev@gmail.com>
2021-10-25 22:15:57 +0200

    add problems 49 and 50

--- helpers.py
+++ helpers.py
@@ -17,7 +17,7 @@ def is_palyndrome(num):
             return True;
         else:
             return False
-        
+
 def to_binary(n):
     return bin(n)[2:]
 
@@ -40,7 +40,7 @@ def divisors(n):
     divs = []
     for i in range(1, n//2+1):
         if n % i == 0:
-            divs.append(i) 
+            divs.append(i)
     return divs
 
 def factorial(n):
@@ -55,3 +55,42 @@ def is_pandigital(num, n):
         if str(i) not in num_str:
             return False
     return True
+
+# Sieve of Eratosthenes
+# Code by David Eppstein, UC Irvine, 28 Feb 2002
+# http://code.activestate.com/recipes/117119/
+
+def gen_primes():
+    """ Generate an infinite sequence of prime numbers.
+    """
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+    # indefinitely, but only as long as required by the current
+    # number being tested.
+    #
+    D = {}
+
+    # The running integer that's checked for primeness
+    q = 2
+
+    while True:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+            #
+            yield q
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next
+            # multiples of its witnesses to prepare for larger
+            # numbers
+            #
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+
